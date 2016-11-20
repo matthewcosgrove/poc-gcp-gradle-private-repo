2ecb105b7e60194bde82f019e7cd9c7ed0ce84c5
matthewcosgrove <mcosgrove@pivotal.io>
2016-11-20 11:22:36 +0000

    Initial commit of test gradle project which can to deploy to a private repo via ./gradlew upload

---
+++ src/main/java/com/mc/poc/PocGoogleCloudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.mc.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocGoogleCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocGoogleCloudDemoApplication.class, args);
+	}
+}
